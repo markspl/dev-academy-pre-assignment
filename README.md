56897c9988287f2275bafc8b173b1876ec72ebb4
Markus Palomäki <markuspalomaki@gmail.com>
2022-08-12 23:38:53 +0300

    Import all CSV files from specific 'dataset' folder

--- README.md
+++ README.md
@@ -7,11 +7,52 @@ This is pre-assignment for Solita Dev Academy Finland (fall 2022).
 > - https://dev.hsl.fi/citybikes/od-trips-2021/2021-06.csv
 > - https://dev.hsl.fi/citybikes/od-trips-2021/2021-07.csv
 
+- - -
+
+### Data validating and checking
+- Validates data using regex
+    - ID Regex: 
+        >`^\d+$`
+        > 
+        > (E.g. "093")
+    - Number Regex:
+        > `^\d+(?:\.\d+)?$`
+        > 
+        > ("215" and "124.4")
+    - Time Regex:
+        > `^[2][0-9]{3}-(0[0-9]|1[0-2])-([012][0-9]|3[0-1])T([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$`
+        > 
+        >("2021-05-21T18:34:11")
+
+- Checks data
+    - If a journey lasted for less than 10 seconds
+    - If a journey covered distance is shorter than 10 meters
+
+- Import and insert lasts ~2min
+
+### Database
+- Uses [SQLite](https://www.sqlite.org/index.html) with `go-sqlite3` driver by [mattn](https://github.com/mattn/go-sqlite3)
+- Creates database table with primary key integer
+    > ```txt
+    > Id                   INTEGER PRIMARY KEY,
+	> Departure            TEXT NOT NULL,
+	> Return               TEXT NOT NULL,
+	> DepartureStationId   TEXT NOT NULL,
+	> DepartureStationName TEXT NOT NULL,
+	> ReturnStationId      TEXT NOT NULL,
+	> ReturnStationName    TEXT NOT NULL,
+	> Distance             TEXT NOT NULL,
+	> Duration             TEXT NOT NULL
+    >```
+
+- - -
+
 ### To-Do
 
+#### Data import
 ##### Recommended
-- [ ] Import data from the CSV files to a database or in-memory storage
-- [ ] Validate data before importing
+- [x] Import data from the CSV files to a database or in-memory storage
+- [x] Validate data before importing
 - [x] Don't import journeys that lasted for less than ten seconds
 - [x] Don't import journeys that covered distances shorter than 10 meters
 
--- backend/main.go
+++ backend/main.go
@@ -10,54 +10,61 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const DBNAME string = "database.db"             // Citybike database name
-const CSVADDRESS string = "dataset/2021-05.csv" // CSV file address (import)
-const MINJOURNEYDIST float64 = 10.0             // Don't import journeys if m < 10m
-const MINJOURNEYTIME int = 10                   // Don't import journeys if t < 10s
+const DBNAME string = "database.db"   // Citybike database name
+const CSVADDRESS string = "./dataset" // CSV file address (import)
+const MINJOURNEYDIST float64 = 10.0   // Don't import journeys if m < 10m
+const MINJOURNEYTIME int = 10         // Don't import journeys if t < 10s
+const STMTCOUNT int = 1000            // How many values are imported to database (max)
 
-func validateDataBeforeImport(data []string, id int) (errValidating bool) {
+func validateDataBeforeImport(data []string, id int) (validated bool) {
+	validated = true
+
 	// Stop if value type is not correct
 	errHandler := func(value string) {
-		fmt.Printf("Validation error: [%v] Value: %v - Not importing row.\n", id, value)
+		// Disabled for faster loading
+		//fmt.Printf("Validation error: [%v] Value: %v - Not importing row.\n", id, value)
 
-		errValidating = false
+		validated = false
 	}
 
 	// Validate values using regex
 	// ID only number [0-9+]
-	regexId, _ := regexp.Compile("^\\d+$")
+	regexId, _ := regexp.Compile(`^\d+$`)
+
 	// Number can have a dot between numbers
-	regexNumber, _ := regexp.Compile("^\\d+(?:\\.\\d+)?$")
+	regexNumber, _ := regexp.Compile(`^\d+(?:\.\d+)?$`)
+
 	// Time and time must be realistic (year 2000-2999) and string includes "T"
 	regexTime, _ := regexp.Compile("^[2][0-9]{3}-(0[0-9]|1[0-2])-([012][0-9]|3[0-1])T([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$")
 
 	// Going thru every value in array
-	if !regexTime.MatchString(data[0]) {
+	if !regexTime.MatchString(data[0]) || data[0] == "" {
 		errHandler(data[0])
 	}
-	if !regexTime.MatchString(data[1]) {
+	if !regexTime.MatchString(data[1]) || data[1] == "" {
 		errHandler(data[1])
 	}
-	if !regexId.MatchString(data[2]) {
+	if !regexId.MatchString(data[2]) || data[2] == "" {
 		errHandler(data[2])
 	}
 	if data[3] == "" {
 		errHandler(data[3])
 	}
-	if !regexId.MatchString(data[4]) {
+	if !regexId.MatchString(data[4]) || data[4] == "" {
 		errHandler(data[4])
 	}
 	if data[5] == "" {
 		errHandler(data[5])
 	}
-	if !regexNumber.MatchString(data[6]) {
+	if !regexNumber.MatchString(data[6]) || data[6] == "" {
 		errHandler(data[6])
 	}
-	if !regexNumber.MatchString(data[7]) {
+	if !regexNumber.MatchString(data[7]) || data[7] == "" {
 		errHandler(data[7])
 	}
 
@@ -65,13 +72,16 @@ func validateDataBeforeImport(data []string, id int) (errValidating bool) {
 }
 
 func main() {
+	// Count how long running this takes
+	timeTrack := time.Now()
+
 	// Remove existing database when launching the server
 	os.Remove("./db/" + DBNAME)
 
 	db, err := sql.Open("sqlite3", "./db/"+DBNAME)
 
 	// If error else than zero value ("uninitialized" value)
-	errorHandler(err)
+	errorHandler(err, "")
 
 	sqlStatement := `
 			CREATE TABLE IF NOT EXISTS Journeys (
@@ -90,90 +100,113 @@ func main() {
 
 	// Create database table
 	_, err = db.Exec(sqlStatement)
-	errorHandler(err)
+	errorHandler(err, "")
 
-	// Open CSV example file
-	file, err := os.Open(CSVADDRESS)
-	errorHandler(err)
+	// Open all CSV example files
+	folder, err := os.Open(CSVADDRESS)
+	errorHandler(err, "")
 
-	read := csv.NewReader(file)
-
-	// Read only the first row
-	_, err = read.Read()
-	errorHandler(err)
+	defer folder.Close()
+	files, err := folder.Readdirnames(0)
+	errorHandler(err, "")
 
 	// Import data from file
-	fmt.Println("Loading...")
+	fmt.Printf("Loading...\n\n")
 	idValue := 0
 
-	for {
-		i := 0
-		stmtEnd := []string{}
-		var errInner error
+	// Go thru all files
+	for _, name := range files {
+		// Filter only CSV files
+		if !strings.HasSuffix(name, ".csv") {
+			fmt.Printf("\nFile %v is not CSV file, skip.", name)
+			break
+		}
 
-		// Create a bulk INSERT with 100 VALUES
-		for i < 100 {
-			r, errInner := read.Read()
+		filePath := fmt.Sprintf("%v/%v", CSVADDRESS, name)
+		file, err := os.Open(filePath)
+		errorHandler(err, "")
 
-			// No more input available
+		defer file.Close()
+
+		fmt.Printf("File %v loaded. Importing...\n", name)
+
+		read := csv.NewReader(file)
+
+		// Read only the first row
+		_, err = read.Read()
+		errorHandler(err, "")
+
+		for {
+			i := 0
+			stmtEnd := []string{}
+			var errInner error
+
+			// Create a bulk INSERT with STMTCOUNT VALUES
+			for i < STMTCOUNT {
+				r, errInner := read.Read()
+
+				// No more input available
+				if errors.Is(errInner, io.EOF) {
+					break
+				}
+
+				// Validate data before importing
+				// If the row includes incorrect values (detected by regex), skip row
+				if validateDataBeforeImport(r, idValue) {
+					// Check if journey lasted for less than 10s and distance over 10m
+					dist, err := strconv.ParseFloat(r[6], 64)
+					errorHandler(err, "")
+					longerTime := (dist > MINJOURNEYDIST)
+					timeA, err := strconv.Atoi(r[7])
+					errorHandler(err, "")
+					longerDist := (timeA > MINJOURNEYTIME)
+
+					if longerTime && longerDist {
+						value := "('" + strconv.Itoa(idValue) + "','" + strings.Join(r, "','") + "')"
+
+						// Include to same array
+						stmtEnd = append(stmtEnd, value)
+
+						// Keep Id unique
+						idValue += 1
+					}
+				}
+			}
+
+			stmtBegin := "INSERT INTO Journeys(Id, Departure, Return, DepartureStationId, DepartureStationName, ReturnStationId, ReturnStationName, Distance, Duration) VALUES"
+
+			completed := stmtBegin + " " + strings.Join(stmtEnd, ",") + ";"
+
+			// Insert data
+			_, err := db.Exec(completed)
+			errorHandler(err, completed)
+
+			// If no input happened, let's quit outer for too.
 			if errors.Is(errInner, io.EOF) {
 				break
 			}
-
-			// Validate data before importing
-			// If the row includes incorrect values (detected by regex), skip row
-			if validateDataBeforeImport(r, idValue) {
-				break
-			}
-
-			// Check if journey lasted for less than 10s and distance over 10m
-			dist, err := strconv.ParseFloat(r[6], 64)
-			errorHandler(err)
-			longerTime := (dist > MINJOURNEYDIST)
-			timeA, err := strconv.Atoi(r[7])
-			errorHandler(err)
-			longerDist := (timeA > MINJOURNEYTIME)
-
-			if longerTime && longerDist {
-				value := "('" + strconv.Itoa(idValue) + "','" + strings.Join(r, "','") + "')"
-
-				// Include to same array
-				stmtEnd = append(stmtEnd, value)
-
-				// Keep Id unique
-				idValue += 1
-			}
-		}
-
-		stmtBegin := "INSERT INTO Journeys(Id, Departure, Return, DepartureStationId, DepartureStationName, ReturnStationId, ReturnStationName, Distance, Duration) VALUES"
-
-		completed := stmtBegin + " " + strings.Join(stmtEnd, ",") + ";"
-
-		// Insert data
-		_, err := db.Exec(completed)
-		errorHandler(err)
-
-		// If no input happened, let's quit outer for too.
-		if errors.Is(errInner, io.EOF) {
 			break
 		}
-		break
 	}
-	fmt.Println("Loaded.")
+
+	fmt.Printf("\n\nLoaded.")
 
 	// Count exported lines
 	var count int
 
 	err = db.QueryRow("SELECT COUNT(*) FROM Journeys").Scan(&count)
-	errorHandler(err)
+	errorHandler(err, "")
 
 	fmt.Println("\nTotal of exported journeys:", count)
 
 	db.Close()
+
+	fmt.Printf("\nTime: %v\n", time.Since(timeTrack))
 }
 
-func errorHandler(err error) {
+func errorHandler(err error, completed string) {
 	if err != nil {
+		fmt.Println(completed)
 		panic(err)
 	}
 }
