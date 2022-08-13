a3e7a1c0fc3ca3b3ad5894e19aeaba3edc9da920
Markus Palomäki <markuspalomaki@gmail.com>
2022-08-13 18:04:42 +0300

    Create API route with simple OK! response

--- backend/go.mod
+++ backend/go.mod
@@ -3,3 +3,5 @@ module backend
 go 1.19
 
 require github.com/mattn/go-sqlite3 v1.14.14
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
-	"backend/database"
+	"github.com/gorilla/mux"
 )
+
+const PORT int = 3000
+
+type Hello struct {
+	Test string
+}
 
 func main() {
 	fmt.Print("\n### Initialize database\n\n")
-	database.InitDatabase()
+	//database.InitDatabase()
+
+	// Initialize new router
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Register routes
+	router.HandleFunc("/api/journeys", getJourneys).Methods("GET")
+
+	// Start API
+	fmt.Print("\n### Launching server\n\n")
+	fmt.Printf("Starting REST API on port %v", PORT)
+	_, err := fmt.Println(http.ListenAndServe(fmt.Sprintf(":%v", PORT), router))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getJourneys(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(Hello{"Ok!"})
 }
