6debe5b1dff0e78695fd3dc28575d6a485f4b24a
Markus Palomäki <markuspalomaki@gmail.com>
2022-08-14 12:50:46 +0300

    Bootstrap Header

--- frontend/src/Layouts/Header.jsx
+++ frontend/src/Layouts/Header.jsx
@@ -2,7 +2,7 @@ import { Bicycle } from "react-bootstrap-icons"
 
 const Header = () => {
     return (
-        <header className="d-flex flex-wrap justify-content-center border-bottom">
+        <header className="d-flex flex-wrap justify-content-center border-bottom shadow-sm">
             <a href="/" className="align-items-center text-dark text-decoration-none">
             <span className="fs-4"><Bicycle /> City Bike App</span>
             </a>
